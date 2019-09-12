49c91acefcab082315e1958c7313138a18b20362
Zaher Mahmoudi <zamahm@devport.local>
2019-09-12 17:01:53 +0200

    Added Splash screen activity

--- app/src/main/java/com/example/barcodetest/WelcomeSplash.kt
+++ app/src/main/java/com/example/barcodetest/WelcomeSplash.kt
@@ -1,4 +1,18 @@
 package com.example.barcodetest
 
-class WelcomeSplash {
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+class WelcomeSplash: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        finish()
+    }
+
+
+
 }
