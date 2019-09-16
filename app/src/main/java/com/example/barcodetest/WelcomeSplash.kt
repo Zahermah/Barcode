bbbc8414dcf356a02e3a0b16f33180a660908483
Zaher Mahmoudi <zamahm@devport.local>
2019-09-16 15:28:45 +0200

    added splash screen animation

--- app/src/main/java/com/example/barcodetest/WelcomeSplash.kt
+++ app/src/main/java/com/example/barcodetest/WelcomeSplash.kt
@@ -1,16 +1,59 @@
 package com.example.barcodetest
 
+import android.animation.Animator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.widget.TextView
+import kotlinx.android.synthetic.main.splash_activity.*
+import org.w3c.dom.Text
 
 class WelcomeSplash : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.splash_activity)
+        showAnimation()
+    }
+
+
+    fun showAnimation() {
+        val spalshTextView: TextView = findViewById(R.id.textTitle)
+        val valueAnimator = ValueAnimator.ofFloat(0f, 1f)
+
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            spalshTextView.scaleX = value
+            spalshTextView.scaleY = value
+        }
+
+        valueAnimator.interpolator = BounceInterpolator()
+        valueAnimator.duration = 1500
+
+        valueAnimator.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationRepeat(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finish()
+
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+
+            }
+
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+
+        })
+
+        valueAnimator.start()
     }
 }
